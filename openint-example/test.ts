292b5942bfe1fbd407240e5c52771b3f466bf6da
Tony Xiao <tonyx.ca@gmail.com>
2024-05-27 13:27:59 -0700

    Test.ts also

---
+++ openint-example/test.ts
@@ -0,0 +1,8 @@
+import { initOpenIntSDK } from '@opensdks/sdk-openint'
+const serverOpenInt = initOpenIntSDK({
+  baseUrl: 'http://localhost:4000/api/v0',
+  headers: { 'x-apikey': process.env.OPENINT_API_KEY },
+})
+const endUserToken = serverOpenInt
+  .POST('/connect/token', { body: { endUserId: 'test' } })
+  .then((r) => r.data.token)
